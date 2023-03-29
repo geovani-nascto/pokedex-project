29dcbfde024865bd4fd47928343c3a221798fb3a
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2023-03-28 23:55:56 -0300

    att requisicao quase completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,45 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
-
-function convertPokemonToHtml(pokemon){
-    return `
-        <li class="pokemon">
-            <span class="number">#001</span>
-            <span class="name">${pokemon.name}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
-                </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
+// function convertPokemonTypesToHtml(pokemonTypes){
+//     return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`);
+// }
 
 const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const limit = 5;
+const offset = 0;
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for(let i = 0; i < pokemons.length; i++){
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToHtml(pokemon);
-        }
+
+function loadPokemonItems(offset, limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+    
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+        '`).join('');
+        pokemonList.innerHTML += newHtml;
+    
+        // const listItems = [];
+        // for(let i = 0; i < pokemons.length; i++){
+        //     const pokemon = pokemons[i];
+        //     listItems.push(convertPokemonToHtml(pokemon));
+        // }
     })
-    .catch((error) => console.error(error))
+}
+
+loadPokemonItems(offset, limit);
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    loadPokemonItems(offset, limit);
+})
+
 
 
 // fetch(url)
